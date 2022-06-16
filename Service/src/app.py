7dd1432a2699cd78c61729efa6fc07fe0fd03e26
Thomas Tomow <toto_san@live.com>
2022-06-16 19:43:07 +0000

    added service template and dockerfile

--- Service/src/app.py
+++ Service/src/app.py
@@ -1,9 +1,18 @@
-from http.client import OK
-from urllib import request, response
 from github_caller import GH
 from flask import Flask, jsonify, redirect, render_template, Response
+
 app = Flask(__name__)
+
+gh = GH()
 
 @app.route("/")
 def index():
     return 'Good', 200
+
+@app.route("/cancel")
+def cancel():
+    return 'Cancelled', 200
+
+@app.route("/ping")
+def ping():
+    return 'Pong', 200
