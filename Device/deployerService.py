ae83d2cd3d88108112a9793322e6cf3da8ec326c
Thomas Tomow <toto_san@live.com>
2022-07-05 14:47:02 +0100

    parked

--- Device/deployerService.py
+++ Device/deployerService.py
@@ -74,6 +74,7 @@ class DeployerService:
             print(res.content)
             #print("High CPU")
             self.log("High CPU", self.RED)
+            self.cancel()
             
     def reject(self):
         for i in self.listOfRuns:
